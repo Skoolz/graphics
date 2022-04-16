2d0dc984e7c23109cd8b3936c7b244caed86fd5a
Skoolz <skoolrus771@gmail.com>
2022-04-16 21:53:37 +0500

    Добавлен README

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -7,65 +7,60 @@
 #include <glm/gtc/type_ptr.hpp>
 using namespace std;
 GLuint VBO;
-struct Matrix4f {
-	float m[4][4];
-};
 glm::mat4 World;
+glm::mat4 WorldPerspect;
+glm::mat4 WorldCamRotate;
+glm::mat4 WorldCamTrans;
+glm::mat4 WorldScale;
+glm::mat4 WorldRotate;
+glm::mat4 WorldTrans;
+
+const float ar = 1024 / 768;
+const float zNear = 0.005;
+const float zFar = 80;
+const float zRange = zNear - zFar;
+const float tanHalfFOV = tanf(glm::radians(20 / 2.0));
+
+
+glm::vec3 CameraTarget(0.45f * sin(Scale), 0.0f, 1.0f);
+glm::vec3 CameraUp(0.0f, 1.0f, 1.0f);
+
 float Scale = 0;
 
 void func() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glm::mat4 WorldMat = glm::mat4{ 1.0f };
-
-	const float ar = 1024 / 768;
-	const float zNear = 0.005;
-	const float zFar = 80;
-	const float zRange = zNear-zFar;
-	const float tanHalfFOV = tanf(glm::radians(20 / 2.0));
 	Scale += 0.001f;
 
-
-	World[0][0] = 1.0f / (tanHalfFOV * ar); World[0][1] = 0; World[0][2] = 0; World[3][0] = 0;
-	World[1][0] = 0; World[1][1] = 1.0f / tanHalfFOV; World[1][2] = 0; World[1][3] = 0.0f;
-	World[2][0] = 0; World[2][1] = 0; World[2][2] = (-zNear - zFar) / zRange; World[2][3] = 1.0f;
-	World[3][1] = 0; World[3][2] = 2.0f * zFar * zNear / zRange; World[3][3] = 0.0f;
-
-	//World[0][0] = 1.0f; World[0][1] = 0; World[0][2] = 0; World[3][0] = 0;
-	//World[1][0] = 0; World[1][1] = 1; World[1][2] = 0; World[1][3] = 0.0f;
-	//World[2][0] = 0; World[2][1] = 0; World[2][2] = 1; World[2][3] = 0.0f;
-	//World[3][1] = 0; World[3][2] = 0; World[3][3] = 1.0f;
-	glm::mat4 World2(0);
-	glm::vec3 CameraPos(0.5, 0, 0);
-
-	World2[0][0] = 1.0f; World2[0][1] = 0; World2[0][2] = 0; World2[0][3] = 0;
-	World2[1][0] = 0; World2[1][1] = 1; World2[1][2] = 0; World2[1][3] = 0;
-	World2[2][0] = 0; World2[2][1] = 0; World2[2][2] = 1; World2[2][3] = 0;
-	World2[3][0] = -CameraPos.x; World2[3][1] = -CameraPos.y; World2[3][2] = -CameraPos.z; World2[3][3] = 1.0f;
-
-	glm::vec3 CameraTarget(0.45f*sin(Scale), 0.0f, 1.0f);
-	glm::vec3 CameraUp(0.0f, 1.0f, 1.0f);
+	CameraTarget = glm::vec3(0.45f * sin(Scale), 0.0f, 1.0f);
 
 	glm::vec3 N = CameraTarget;
 	N = glm::normalize(N);
 	glm::vec3 U = CameraUp;
 	U = glm::normalize(U);
 	U = glm::cross(U, CameraTarget);
-	glm::vec3 V = glm::cross(N,U);
+	glm::vec3 V = glm::cross(N, U);
 
-	glm::mat4 World3(0);
+	WorldCamRotate= glm::mat4{
+		U.x,V.x,N.x,0.0f,
+		U.y,V.y,N.y,0.0f,
+		U.z,V.z,N.z,0.0f,
+		0.0f,0.0f,0.0f,1.0f,
+	};
 
-	World3[0][0] = U.x; World3[0][1] = V.x; World3[0][2] = N.x; World3[0][3] = 0.0f;
-	World3[1][0] = U.y; World3[1][1] = V.y; World3[1][2] = N.x; World3[1][3] = 0.0f;
-	World3[2][0] = U.z; World3[2][1] = V.z; World3[2][2] = N.z; World3[2][3] = 0.0f;
-	World3[3][0] = 0.0f; World3[3][1] = 0.0f; World3[3][2] = 0.0f; World3[3][3] = 1;
+	WorldTrans[0][3] = sinf(Scale);
+
+	WorldRotate[0][0] = cosf(Scale);
+	WorldRotate[1][0] = sinf(Scale);
+	WorldRotate[0][1] = -sinf(Scale);
+	WorldRotate[1][1] = cosf(Scale);
+
+	WorldScale[0][0] = abs(sinf(Scale));
+	WorldScale[1][1] = abs(cosf(Scale));
+	WorldScale[2][2] = abs(sinf(Scale));
 
 
-
-
-
-
-	glm::mat4 Worldmat2 = World*World3*World2* WorldMat;
+	glm::mat4 WorldMat2 = WorldPerspect * WorldCamRotate * WorldCamTrans * WorldScale * WorldRotate * WorldTrans * WorldMat;
 
 
 
@@ -103,22 +98,66 @@ int main(int argc, char** argv)
 		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
 		return 1;
 	}
-	//glm::vec3 Vertices[1];
-	//Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
-
-	//glm::vec3 Vertices[6];
-	//Vertices[0] = glm::vec3(0, 0, 10);
-	//Vertices[1] = glm::vec3(0, 0.5, 10);
-	//Vertices[2] = glm::vec3(0.5, 0, 10);
-
-	//Vertices[3] = glm::vec3(-0.7, 0, 20);
-	//Vertices[4] = glm::vec3(-0.7, 0.5, 20);
-	//Vertices[5] = glm::vec3(-0.7+0.5, 0, 20);
 
 	glm::vec3 Vertices[3];
 	Vertices[0] = glm::vec3(0, 0, 10);
 	Vertices[1] = glm::vec3(0, 0.5, 10);
 	Vertices[2] = glm::vec3(0.5, 0, 10);
+
+
+
+	CameraTarget = glm::vec3(0.45f * sin(Scale), 0.0f, 1.0f);
+
+	glm::vec3 N = CameraTarget;
+	N = glm::normalize(N);
+	glm::vec3 U = CameraUp;
+	U = glm::normalize(U);
+	U = glm::cross(U, CameraTarget);
+	glm::vec3 V = glm::cross(N, U);
+
+	WorldMat = glm::mat4{ 1.0f };
+
+	WorldTrans = glm::mat4{
+	1.0f, 0.0f,  0.0f, 0.0f,
+	0.0f, 1.0f,  0.0f, 0.0f,
+	0.0f, 0.0f,  1.0f, 0.0f,
+	sinf(Scale), 0.0f, 0.0f, 1.0f
+	};
+
+	WorldRotate = glm::mat4{
+	cosf(Scale),  sinf(Scale), 0.0f, 0.0f,
+	-sinf(Scale), cosf(Scale), 0.0f, 0.0f,
+	 0.0f,        0.0f,        1.0f, 0.0f,
+	 0.0f,        0.0f,        0.0f, 1.0f
+	};
+
+	WorldScale = glm::mat4{
+	 abs(sinf(Scale)), 0.0f, 0.0f, 0.0f,
+	 0.0f, abs(cosf(Scale)), 0.0f, 0.0f,
+	 0.0f, 0.0f, abs(sinf(Scale)), 0.0f,
+	 0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	WorldPerspect = glm::mat4{
+		1.0f / (tanHalfFOV * ar), 0.0f,             0.0f, 0.0f,
+		0.0f,                1.0f / tanHalfFOV,      0.0f, 0.0f,
+		0.0f, 0.0f,          (-zN - zF) / zR,  2.0f * zF * zN / zR,
+		0.0f, 0.0f,          1.0f,              0.0f
+	};
+
+	WorldCamTrans = glm::mat4{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+	   -cameraPos.x, -cameraPos.y, -cameraPos.z, 1.0f,
+	};
+
+	WorldCamRotate= glm::mat4{
+	U.x,V.x,N.x,0.0f,
+	U.y,V.y,N.y,0.0f,
+	U.z,V.z,N.z,0.0f,
+	0.0f,0.0f,0.0f,1.0f,
+	};
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 
+# Лабораторная работа № 2 
+## 1) Изучение и написание матриц перемещения, вращения и изменения размера объекта:
+### Инициализация переменной для изменения 
+```с++
+float Scale = 0.0f;
+```
+### Задание матриц
+```с++
+
+    //Базовая матрица
+    glm::mat4 WorldMat = glm::mat4{ 1.0f };
+
+    //матрица перемещения
+    glm::mat4 WorldTrans{
+    1.0f, 0.0f,  0.0f, 0.0f,
+    0.0f, 1.0f,  0.0f, 0.0f,
+    0.0f, 0.0f,  1.0f, 0.0f,
+    sinf(Scale), 0.0f, 0.0f, 1.0f
+    };
+
+    //Матрица вращения
+    glm::mat4 WorldRotate{
+    cosf(Scale),  sinf(Scale), 0.0f, 0.0f,
+    -sinf(Scale), cosf(Scale), 0.0f, 0.0f,
+     0.0f,        0.0f,        1.0f, 0.0f,
+     0.0f,        0.0f,        0.0f, 1.0f
+    };
+
+    //Матрица изменения масштаба
+    glm::mat4 WorldScale{
+     abs(sinf(Scale)), 0.0f, 0.0f, 0.0f,
+     0.0f, abs(cosf(Scale)), 0.0f, 0.0f,
+     0.0f, 0.0f, abs(sinf(Scale)), 0.0f,
+     0.0f, 0.0f, 0.0f, 1.0f
+     };
+     
+     Scale += 0.001f;
+``` 
+
+Для постоянного обновления значения Scale, в функции отрисовки необходимо добавить вызов функции:
+```c++
+glutPostRedisplay();
+```
+
+## 2) Проекция перспективы
+### инициализвция необходимых переменных
+```с++
+const float ar = 1024 / 768;
+const float zNear = 0.005;
+const float zFar = 80;
+const float zRange = zNear-zFar;
+const float tanHalfFOV = tanf(glm::radians(20 / 2.0));
+```
+###  Задание матрицы перспективы
+```с++
+glm::mat4 WorldPerspect{
+        1.0f / (tanHalfFOV * ar), 0.0f,             0.0f, 0.0f,
+        0.0f,                1.0f / tanHalfFOV,      0.0f, 0.0f,
+        0.0f, 0.0f,          (-zN - zF) / zR,  2.0f * zF * zN / zR,
+        0.0f, 0.0f,          1.0f,              0.0f
+    };
+```
+## 3) Пространство камеры
+###  Задание системы координат камеры. Позиция камеры и 3 вектора камеры
+```с++
+ glm::vec3 cameraPos = glm::vec3(0.3f, 0.3f, 0.3f);
+
+ glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+ glm::vec3 cameraTarget = glm::vec3(0.45f, 0.0f, 1.0f);
+ glm::vec3 cameraRight = glm::cross(cameraUp, cameraTarget);
+```
+###  Задание матрицы изменения позиции камеры
+```с++
+glm::mat4 WorldCamTrans{
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+       -cameraPos.x, -cameraPos.y, -cameraPos.z, 1.0f,
+    };
+```
+### Задание матрицы вращения камеры
+```с++
+glm::vec3 N = cameraTarget;
+    N = glm::normalize(N);
+    glm::vec3 U = cameraUp;
+    U = glm::normalize(U);
+    U = glm::cross(U, cameraTarget);
+    glm::vec3 V = glm::cross(N, U);
+    
+    glm::mat4 WorldCamRotate{
+        U.x,V.x,N.x,0.0f,
+        U.y,V.y,N.y,0.0f,
+        U.z,V.z,N.z,0.0f,
+        0.0f,0.0f,0.0f,1.0f,
+    };
+```
+## 4) Применение всех преобразований
+```с++
+glm::mat4 WorldMat2 = WorldPerspect * WorldCamRotate *          WorldCamTrans * WorldScale * WorldRotate * WorldTrans * WorldMat;
+glLoadMatrixf(static_cast<const float*>(glm::value_ptr(WorldMat2)));
+```
